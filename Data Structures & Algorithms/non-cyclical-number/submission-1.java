4806c2ca7bc607322a7d1748775a5dbf29a7388d
StackTraced <64375555+StackTraced@users.noreply.github.com>
2026-04-11 20:13:00 -0400

    Add: non-cyclical-number - submission-1

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isHappy(int n) {
+        
+        HashSet<Integer> seen = new HashSet<>();
+
+        while(true) {
+            n = getTheValue(n);
+            if (n == 1) return true;
+
+            if (seen.contains(n)) return false;
+
+            seen.add(n);
+        }
+
+        // True until we find a contradiction
+        //return true;
+    }    
+
+    private int getTheValue(int n) {
+        // We want to chop it down and grab its value
+        int sum = 0;
+
+
+        while (n > 0) {
+            int value = n % 10;
+            sum += Math.pow(value, 2);
+            n /= 10;
+        }
+        System.out.println("Sum: " + sum);
+        return sum;
+    }
+}
