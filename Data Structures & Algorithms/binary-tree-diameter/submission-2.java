92d749af6683294ca6ec4306dfcfd3f5ca600fc8
StackTraced <64375555+StackTraced@users.noreply.github.com>
2026-05-05 12:24:12 -0400

    Add: binary-tree-diameter - submission-2

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-2.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    private int bestDistance = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        dfs(root);
+
+        return bestDistance;
+    }
+
+    // We want to return the best of the two paths, but also check the best
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+
+        int leftNode = dfs(root.left);
+        int rightNode = dfs(root.right);
+
+        //System.out.println("At node: " + root.val + " the left is " + leftNode + " and the right is: " + rightNode);
+
+        bestDistance = Math.max(leftNode + rightNode, bestDistance);
+
+        return Math.max(leftNode, rightNode) + 1;
+    }
+}
