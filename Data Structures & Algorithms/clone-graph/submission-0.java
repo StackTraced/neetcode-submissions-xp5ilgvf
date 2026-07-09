c9be151803de243bcc1408b5e3c5e3a1a661bcd4
StackTraced <64375555+StackTraced@users.noreply.github.com>
2026-07-09 14:30:37 -0400

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.java
@@ -0,0 +1,61 @@
+/*
+Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+// Steps
+// Have we 
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        HashMap<Node, Node> map = new HashMap<>();
+
+        deepCopy(node, map);
+
+        return map.get(node);
+    }
+
+    private void deepCopy(Node node, HashMap<Node, Node> map) {
+        System.out.println("Size of list: " + map.size());
+        
+        // If we have seen this before, we return
+        if (map.containsKey(node)) {
+            return;
+        }
+
+        // This is a brand new node we're in, create it
+        map.put(node, clonedNodeValues(node));
+
+        // Now that we are in here, we want to iterate it's neighbors
+        for(Node n: node.neighbors) {
+            deepCopy(n, map);
+
+            // Grab the cloned copy
+            Node cloned = map.get(node);
+            Node neighborClone = map.get(n);
+            
+            cloned.neighbors.add(neighborClone);
+        }
+    }
+
+    
+    private Node clonedNodeValues(Node node) {
+        return new Node(node.val, new ArrayList<>());
+    }
+}
