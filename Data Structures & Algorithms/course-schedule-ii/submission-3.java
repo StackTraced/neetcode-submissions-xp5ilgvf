87f813abce45faa29c03ae4a3f300ce94b5fddc0
StackTraced <64375555+StackTraced@users.noreply.github.com>
2026-07-13 18:01:33 -0400

    Add: course-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-3.java
@@ -0,0 +1,65 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<Integer> result = new ArrayList<>();
+
+
+        // Build the map
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+
+        boolean[] completed = new boolean[numCourses];
+
+        for(int i = 0; i < numCourses; i++) {
+            map.put(i, new HashSet<>());
+        }
+
+        for (int[] i: prerequisites) {
+            map.get(i[0]).add(i[1]);
+        }
+
+        for (int i = 0; i < numCourses; i++) {
+            if (!dfs(i, map, result, new HashSet<>(), completed)) return new int[0];
+        }
+
+        // IF we hit a false above, it already gives us an empty set
+
+        return convert(result, numCourses);
+    }
+
+
+    private boolean dfs(int c, Map<Integer, Set<Integer>> course, List<Integer> result,
+    Set<Integer> seen, boolean[] completed) {
+
+        // On top
+        if (completed[c]) return true; // Its false by default, can only be true if valid
+
+        if (seen.contains(c)) return false; // A cycle? 
+
+        Set<Integer> pre = course.get(c);
+        // Base case to add a course thats empty
+
+        
+        seen.add(c);
+
+        for(int i: pre) {
+            if (!dfs(i, course, result, seen, completed)) return false;
+        }
+
+        seen.remove(c);
+
+        result.add(c);
+        completed[c] = true;
+
+        return true;
+    }
+
+
+    private int[] convert(List<Integer> result, int courses) {
+        int[] arr = new int[courses];
+
+        for(int i = 0; i < courses; i++) {
+            arr[i] = result.get(i);
+        }
+
+        return arr;
+    }
+}
